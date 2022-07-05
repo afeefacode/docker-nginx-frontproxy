c0306df746cbc26165f4b312dc63e7aacc3fd1f2
Kaken Bok <bok@kakenbok.de>
2022-07-05 13:47:10 +0200

    format code

--- cli/Actions/GenerateCert.php
+++ cli/Actions/GenerateCert.php
@@ -21,36 +21,36 @@ class GenerateCert extends Action
         $caFilename = Path::join($caPath, 'ca');
 
         $certPath = Path::join(getcwd(), 'servers', 'certs');
-        $certFilename = Path::join($certPath, "$serverName");
+        $certFilename = Path::join($certPath, "{$serverName}");
 
         $extConfTemplate = Path::join(__DIR__, '..', 'templates', 'ext.conf');
         $extConf = Path::join(__DIR__, '..', 'templates', 'ext_issue.conf');
-        $altName = "DNS:$serverName,DNS:*.$serverName";
+        $altName = "DNS:{$serverName},DNS:*.{$serverName}";
 
-        $str=file_get_contents($extConfTemplate);
-        $str=str_replace('[altName]',$altName,$str);
+        $str = file_get_contents($extConfTemplate);
+        $str = str_replace('[altName]', $altName, $str);
         file_put_contents($extConf, $str);
 
         $this->runProcesses([
-            "openssl genrsa -out $certFilename.key 2048",
+            "openssl genrsa -out {$certFilename}.key 2048",
 
-            "openssl req -subj '/CN=$serverName' -sha256 -new
-                -key $certFilename.key -out $certFilename.csr",
+            "openssl req -subj '/CN={$serverName}' -sha256 -new
+                -key {$certFilename}.key -out {$certFilename}.csr",
 
             "openssl x509 -req -days 900 -sha256
-                    -in $certFilename.csr
-                    -CA $caFilename.pem
-                    -CAkey $caFilename.key
+                    -in {$certFilename}.csr
+                    -CA {$caFilename}.pem
+                    -CAkey {$caFilename}.key
                     -CAcreateserial
-                    -CAserial $certFilename.seq
-                    -out $certFilename.crt
-                    -extfile $extConf",
-            "rm $certFilename.csr",
-            "rm $certFilename.seq",
-            "rm $extConf"
+                    -CAserial {$certFilename}.seq
+                    -out {$certFilename}.crt
+                    -extfile {$extConf}",
+            "rm {$certFilename}.csr",
+            "rm {$certFilename}.seq",
+            "rm {$extConf}"
         ]);
 
-        $relativePath = Path::makeRelative("$certFilename.crt", getcwd());
-        $this->printBullet("<info>Finish</info>: created $relativePath");
+        $relativePath = Path::makeRelative("{$certFilename}.crt", getcwd());
+        $this->printBullet("<info>Finish</info>: created {$relativePath}");
     }
 }
